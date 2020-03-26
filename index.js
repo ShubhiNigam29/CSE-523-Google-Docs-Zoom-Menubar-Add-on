f3f972c3c6132458fd7e3beefaa5fb8bfc5fc979
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2020-03-26 00:32:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+let page ;
+
+async function GetBrowserPage ()  { 
+  
+  const browser  =  await  puppeteer.launch ({args:['--no-sandbox']}); 
+  return browser.newPage(); 
+}
+
+exports.getDOM  =  async (req, res) => {
+
+  if(!page) { 
+    page  =  await  GetBrowserPage (); 
+  }
+
+  await page.goto('https://docs.google.com/document/d/1VFaU79lhqmpE4F_q-tZa3P3Qtj2FY_YigA2eywTeAdY/edit');
+
+  const result  =  await page.content();
+  //console.log(result);
+  res.set ('Content-Type','text/html'); 
+  res.send(result); 
+};
