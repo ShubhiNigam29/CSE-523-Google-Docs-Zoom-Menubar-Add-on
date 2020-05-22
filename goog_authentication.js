7893ac5a36fb3a8516f220c42521d0b22606cce1
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2020-05-22 03:36:43 -0400

    Puppeteer Google Auth (OTP request/Captcha issue)
    Puppeteer Google Auth (OTP request/Captcha issue)

--- goog_authentication.js
+++ goog_authentication.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
         headless: false
     });
     const page = await browser.newPage();
-    //await page.setUserAgent('Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36');
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36');
     await page.setViewport({
         width: 1920,
         height: 1080
@@ -33,7 +33,7 @@ const puppeteer = require('puppeteer');
     await page.click('input[type=submit]');
     await page.waitFor(2000);
     // console.log(await page.content());
-    //await page.select('#countryList','US');
+    await page.select('#countryList','US');
     await page.click('input[type=submit]');
     await page.screenshot({
         path: 'example3.png'
